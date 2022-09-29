bb76ec8da25fbce5cc18f9cff0fb347196c86c0c
ErikaOjeda22 <erika.ojeda.garnica@gmail.com>
2022-09-29 02:30:32 -0500

    prueba

--- mysite/settings.py
+++ mysite/settings.py
@@ -78,8 +78,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'NAME':'/home/miexamenErikaOjeda/miexamenerikaojeda.pythonanywhere.com',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
